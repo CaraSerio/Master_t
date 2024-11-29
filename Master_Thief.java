75a8e94f0c27ce9ea7117c7c965927153d0bb720
Adriel Alves Ferreira de Souza <adriel.a2006@aluno.ifsc.edu.br>
2024-11-28 22:12:46 -0300

    mudança de cor

--- Master_Thief.java
+++ Master_Thief.java
@@ -18,7 +18,7 @@ public class Master_Thief extends Robot
 		// After trying out your robot, try uncommenting the import at the top,
 		// and the next line:
 
-		 setColors(Color.white,Color.yellow,Color.black); // body,gun,radar
+		 setColors(Color.white,Color.red,Color.black); // body,gun,radar
 
 		// Robot main loop
 		while(true) {
