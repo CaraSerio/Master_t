dd4caee39c749143ff7e8de6f279ae70ec492c25
Adriel Alves Ferreira de Souza <adriel.a2006@aluno.ifsc.edu.br>
2024-12-19 21:00:53 -0300

    Limpei o codigo

--- Master_Thief.java
+++ Master_Thief.java
@@ -1,4 +1,4 @@
-package Diogo_Lindo;
+package master_t;
 import robocode.*;
 import java.awt.Color;
 import robocode.Robot;
@@ -9,28 +9,18 @@ import static robocode.util.Utils.normalRelativeAngleDegrees;
 
 import java.awt.*;
 
-// API help : https://robocode.sourceforge.io/docs/robocode/robocode/Robot.html
 
-/**
- * Nabucodonosor - a robot by (your name here)
- */
 public class Nabucodonosor extends AdvancedRobot
 {
-   /*
-	 * run: Master_Thief's default behavior
-	 */
+  
 int roboDetectado = 0;
    
 	public void run() {
-		// Initialization of the robot should be put here
-
-		// After trying out your robot, try uncommenting the import at the top,
-		// and the next line:
+		
 
 		 setColors(Color.black,Color.magenta,Color.yellow); // body,gun,radar
 
-		// Robot main loop
-		// Robot main loop
+	
 		while(true) {
 			if(roboDetectado == 0){
 			setAhead(100);
@@ -41,9 +31,7 @@ int roboDetectado = 0;
 		}
 	}
 
-	/**
-	 * onScannedRobot: What to do when you see another robot
-	 */
+
 	public void onScannedRobot(ScannedRobotEvent  e) {
 		
 			double absoluteBearing = getHeading() + e.getBearing();
@@ -65,19 +53,15 @@ int roboDetectado = 0;
 			scan();
 			}
 		}
-	/**
-	 * onHitByBullet: What to do when you're hit by a bullet
-	 */
+
 	public void onHitByBullet(HitByBulletEvent e) {
-		// Replace the next line with any behavior you would like
+	
 		back(10);
 	}
 	
-	/**
-	 * onHitWall: What to do when you hit a wall
-	 */
+
 	public void onHitWall(HitWallEvent e) {
-		// Replace the next line with any behavior you would like
+	
 		setBack(25);
 		setTurnLeft(45);
 		execute();
